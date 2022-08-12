5015df722028c356d45021182b5388b56c6cf67c
Vlad RIABCHENKO <riabchenko@laposte.net>
2022-08-12 17:19:21 +0200

    [FRONT] use ts

---
+++ front/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './screens/home/Home'
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
