80b79008a7c83b2933901cd3c5368afe6494431d
Vlad RIABCHENKO <riabchenko@laposte.net>
2022-08-16 21:30:17 +0200

    docker and back tests

---
+++ README.md
@@ -0,0 +1,6 @@
+#### Launch with docker
+
+Make sure that ports *3000* and *8080* are free and run: 
+```
+docker-compose up -d
+```
--- api/src/test/java/personality/test/api/ApiApplicationTests.java
+++ api/src/test/java/personality/test/api/ApiApplicationTests.java
@@ -1,13 +1,39 @@
 package personality.test.api;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+		classes = ApiApplication.class)
+@ContextConfiguration
+@AutoConfigureMockMvc
+@ActiveProfiles("dev")
 class ApiApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
-	void contextLoads() {
+	void fetchTests() throws Exception {
+		mvc.perform(get("/tests")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().json("{\"_embedded\": {\"tests\": [{\"title\": \"Are you an introvert or an extrovert?\"}]}}"))
+				.andDo(MockMvcResultHandlers.print());
 	}
 
 }
